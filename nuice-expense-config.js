8d4b355204186bb4f0c36f95beb9023e2f6033ad
Umick1717 <w203_brabus_1717@windowslive.com>
2026-08-11 12:23:43 +0700

    Add NuIce expense tracker config

---
+++ nuice-expense-config.js
@@ -0,0 +1,11 @@
+/* My Expense Tracker 2026 Nu'Ice - Public configuration
+ *
+ * หลัง Deploy NuIce_Expense_Code.gs เป็น Google Apps Script Web App
+ * ให้นำ URL ที่ลงท้ายด้วย /exec มาใส่ใน webAppUrl
+ */
+window.NUICE_EXPENSE_CONFIG = Object.freeze({
+  webAppUrl: "",
+  spreadsheetId: "1_G5dDFWUcAb_dANQ8GVF5VL6v3QDTOxhJD8Fc0nUPos",
+  sheetId: 34336380,
+  trackerName: "My Expense Tracker 2026 Nu'Ice"
+});
