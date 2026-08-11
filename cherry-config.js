e74a27aa438be091b6cd22fcc7fdbd44d5420807
Umick1717 <w203_brabus_1717@windowslive.com>
2026-08-11 12:24:55 +0700

    Load dedicated NuIce expense tracker route

--- cherry-config.js
+++ cherry-config.js
@@ -25,3 +25,15 @@ window.CHERRY_CONFIG = Object.freeze({
   script.dataset.expenseMenuLoader = "v13";
   document.head.appendChild(script);
 })();
+
+/* Nu'Ice dedicated Expense page V14 route */
+(() => {
+  if (window.__NUICE_EXPENSE_MENU_V14_LOADER__) return;
+  window.__NUICE_EXPENSE_MENU_V14_LOADER__ = true;
+
+  const script = document.createElement("script");
+  script.src = "expense-menu-v14-nuice.js?v=14";
+  script.async = false;
+  script.dataset.expenseNuIceLoader = "v14";
+  document.head.appendChild(script);
+})();
